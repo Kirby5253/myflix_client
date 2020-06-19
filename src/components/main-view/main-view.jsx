a656c9a729491c72264366e26fd139cc2b96ae45
Jacob Kirby <65231998+Kirby5253@users.noreply.github.com>
2020-06-18 19:02:43 -0500

    added mainview component

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -15,3 +15,34 @@ class MainView extends React.Component{
     );
   }
 }
+
+export class MainView extends React.Component {
+  componentDidMount() {
+    axios.get('<my-api-endpoint/movies')
+      .then(response => {
+        this.setState({
+          movies:response.data
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  render () {
+    // before the data is initially loaded
+    const {movies} = this.state;
+
+    // Before the movies have been loaded
+    if (!movies) return <div className="main-view"/>;
+
+    return (
+      <div className="main-view">
+        {movies.map(movie => (
+          <div className="movie-card" key={movie._id}>{movie.Title}</div>
+        ))}
+      </div>
+    );
+  }
+}
+
