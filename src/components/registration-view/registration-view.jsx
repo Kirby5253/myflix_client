856552a4e6252eb31b96133d68df194b14df0385
Jacob Kirby <65231998+Kirby5253@users.noreply.github.com>
2020-06-22 11:37:24 -0500

    added registration view

---
+++ src/components/registration-view/registration-view.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+export function RegistrationView(props) {
+	const [ newUsername, setNewUsername ] = useState('');
+	const [ newPassword, setNewPassword ] = useState('');
+	const [ newEmail, setNewEmail ] = useState('');
+	const [ newBirthDate, setNewBirthDate ] = useState('');
+
+	// Has ability to login with random credentials for existing user, no functionality for new users yet
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(newUsername, newPassword, newEmail, newBirthDate);
+		// Send a request to the server for authentication then call props.onLoggedIn(username)
+		props.onLoggedIn(newUsername);
+	};
+
+	return (
+		<Form>
+			<Form.Label>
+				<h3>Create New User for MyFlix</h3>
+			</Form.Label>
+
+			<Form.Group controlId="formBasicUsername">
+				<Form.Label>Create Username:</Form.Label>
+				<Form.Control
+					type="text"
+					placeholder="Username"
+					value={newUsername}
+					onChange={(e) => setNewUsername(e.target.value)}
+				/>
+			</Form.Group>
+
+			<Form.Group controlId="formBasicPassword">
+				<Form.Label>Create Password</Form.Label>
+				<Form.Control
+					type="password"
+					placeholder="Password"
+					value={newPassword}
+					onChange={(e) => setNewPassword(e.target.value)}
+				/>
+			</Form.Group>
+
+			<Form.Group controlId="formBasicPassword">
+				<Form.Label>Email</Form.Label>
+				<Form.Control
+					type="email"
+					placeholder="Email"
+					value={newEmail}
+					onChange={(e) => setNewEmail(e.target.value)}
+				/>
+			</Form.Group>
+
+			<Form.Group controlId="formBasicPassword">
+				<Form.Label>Date of Birth</Form.Label>
+				<Form.Control
+					type="text"
+					placeholder="YYYY-MM-DD"
+					value={newBirthDate}
+					onChange={(e) => setNewBirthDate(e.target.value)}
+				/>
+			</Form.Group>
+
+			<Button type="button" onClick={handleSubmit}>
+				Submit
+			</Button>
+		</Form>
+	);
+}
