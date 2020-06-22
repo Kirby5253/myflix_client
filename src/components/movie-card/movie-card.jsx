64c27dc40f586aabc30c908623cc52058dd2b2f7
Jacob Kirby <65231998+Kirby5253@users.noreply.github.com>
2020-06-22 11:39:36 -0500

    added style to movie-card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,22 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import './movie-card.scss';
 
 export class MovieCard extends React.Component {
 	render() {
 		const { movie, onClick } = this.props;
 
 		return (
-			<Card style={{ width: '16rem' }}>
-				<Card.Img variant="top" src={movie.ImagePath} />
-				<Card.Body>
-					<Card.Title>{movie.Title}</Card.Title>
-					<Card.Text>{movie.Description}</Card.Text>
-					<Button onClick={() => onClick(movie)} variant="link">
-						Open
-					</Button>
-				</Card.Body>
-			</Card>
+			<Container className="movie-cards">
+				<Card text={'white'} style={{ width: '16rem' }} bg={'dark'}>
+					<Card.Img variant="top" src={movie.ImagePath} />
+					<Card.Body>
+						<Card.Title>{movie.Title}</Card.Title>
+						<Card.Text>{movie.Description}</Card.Text>
+						<Button onClick={() => onClick(movie)} variant="link">
+							Open
+						</Button>
+					</Card.Body>
+				</Card>
+			</Container>
 		);
 	}
 }
@@ -26,7 +30,7 @@ MovieCard.PropTypes = {
 	movie: PropTypes.shape({
 		Title: PropTypes.string.isRequired,
 		Description: PropTypes.string.isRequired,
-		ImagePath: PropTypes.string
+		ImagePath: PropTypes.string.isRequired
 	}).isRequired,
 	onClick: PropTypes.func.isRequired
 };
